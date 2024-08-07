d697210cd8d61f036170d81108aff57febad1334
martin1906s <martin.simbana007@gamil.com>
2024-08-07 14:29:13 -0500

    Función valorSubtotal exitoso

---
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,19 @@
+function calcularValorDescuento(monto,porcentajeDescuento){
+    let valorDescuento=(monto*porcentajeDescuento)/100;
+    return valorDescuento;
+}
+
+function calcularIVA(monto){
+    let valorIVA=monto*0.12;
+    return valorIVA;
+}
+
+function calcularSubtotal(precio,cantidad){
+    let tot=precio*cantidad;
+    return tot;
+}
+
+function calcularTotal(subtotal,descuento,iva){
+    let valorTotal=(subtotal-descuento)+iva;
+    return valorTotal;
+}
