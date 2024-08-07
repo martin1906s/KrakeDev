97b3fe8365d2d17fa96475295e9b948c74ea83f9
martin1906s <martin.simbana007@gamil.com>
2024-08-06 20:44:29 -0500

    El boton modificar imagen

--- Ejercicio_Git/ejercicio.js
+++ Ejercicio_Git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImgSaludo;
+    cmpImgSaludo=document.getElementById("imgSaludo");
+    cmpImgSaludo.src="./Imagenes/bruja.gif";
     console.log("Ingresar a saludar");
 }
