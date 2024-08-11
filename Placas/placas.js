9988d833a9f2e70f9291b27501e7b5dc1c2d8bd7
martin1906s <martin.simbana007@gamil.com>
2024-08-11 01:11:03 -0500

    Reto35: Implementación de provincias

--- Placas/placas.js
+++ Placas/placas.js
@@ -1,10 +1,13 @@
 function validarPlaca() {
+    let provincia;
     let erroresEstructura;
     recuperarTexto("txtPlaca");
     erroresEstructura=validarEstructura("txtPlaca");
     if (erroresEstructura==null) {
+        provincia=obtenerProvincia("txtPlaca")
         mostrarTexto("lblMensajeErrores","PLACA: VALIDA");
         mostrarTexto("lblErrores","")
+        mostrarTexto("nomProvincia",provincia)
         
     }else{
         mostrarTexto("lblMensajeErrores","PLACA: INCORRECTA");
--- Placas/servicioPlacas.js
+++ Placas/servicioPlacas.js
@@ -1,40 +1,124 @@
 function validarEstructura(placa) {
-    placa=recuperarTexto("txtPlaca");
-    let errores="";
-    if(placa.length!=7  && placa.length!=8){
-        errores+="Longitud de la placa incorrecta. \n";
+    placa = recuperarTexto("txtPlaca");
+    let errores = "";
+    if (placa.length != 7 && placa.length != 8) {
+        errores += "Longitud de la placa incorrecta. \n";
     }
-    if(!esMayuscula(placa.charAt(0))){
-        errores+="El primer caracter es una letra mayúscula. \n";
+    if (!esMayuscula(placa.charAt(0))) {
+        errores += "El primer caracter es una letra mayúscula. \n";
     }
-    if(!esMayuscula(placa.charAt(1))){
-        errores+="El segundo caracter es una letra mayúscula. \n";
+    if (!esMayuscula(placa.charAt(1))) {
+        errores += "El segundo caracter es una letra mayúscula. \n";
     }
-    if(!esMayuscula(placa.charAt(2))){
-        errores+="El tercer caracter es una letra mayúscula. \n";
+    if (!esMayuscula(placa.charAt(2))) {
+        errores += "El tercer caracter es una letra mayúscula. \n";
     }
-    if(!esGuion(placa.charAt(3))){
-        errores+="El cuarto caracter es un guión. \n";
+    if (!esGuion(placa.charAt(3))) {
+        errores += "El cuarto caracter es un guión. \n";
     }
-    if(!esDigito(placa.charAt(4))){
-        errores+="El quinto caracter es un digito. \n";
+    if (!esDigito(placa.charAt(4))) {
+        errores += "El quinto caracter es un digito. \n";
     }
-    if(!esDigito(placa.charAt(5))){
-        errores+="El sexto caracter es un digito. \n";
+    if (!esDigito(placa.charAt(5))) {
+        errores += "El sexto caracter es un digito. \n";
     }
-    if(!esDigito(placa.charAt(6))){
-        errores+="El septimo caracter es un digito. \n";
+    if (!esDigito(placa.charAt(6))) {
+        errores += "El septimo caracter es un digito. \n";
     }
-    if(placa.length==8){
-        if(!esDigito(placa.charAt(7))){
-            errores+="El octavo caracter es un digito. \n";
+    if (placa.length == 8) {
+        if (!esDigito(placa.charAt(7))) {
+            errores += "El octavo caracter es un digito. \n";
         }
     }
 
-    if(errores.length>0){
+    if (errores.length > 0) {
         return errores;
-    }else{
+    } else {
         return null;
     }
 
+}
+
+function obtenerProvincia(placa) {
+    let nomProvincia;
+    placa = recuperarTexto("txtPlaca");
+    if (placa.charCodeAt(0) == 65) {
+        nomProvincia = "Azuay";
+    }
+    if (placa.charCodeAt(0) == 66) {
+        nomProvincia = "Bolívar";
+    }
+    if (placa.charCodeAt(0) == 67) {
+        nomProvincia = "Carchi";
+    }
+    if (placa.charCodeAt(0) == 68) {
+        nomProvincia = null;
+    }
+    if (placa.charCodeAt(0) == 69) {
+        nomProvincia = "Esmeraldas";
+    }
+    if (placa.charCodeAt(0) == 70) {
+        nomProvincia = null;
+    }
+    if (placa.charCodeAt(0) == 71) {
+        nomProvincia = "Guayaquil";
+    }
+    if (placa.charCodeAt(0) == 72) {
+        nomProvincia = "Chimborazo";
+    }
+    if (placa.charCodeAt(0) == 73) {
+        nomProvincia = "Imbabura";
+    }
+    if (placa.charCodeAt(0) == 74) {
+        nomProvincia = null;
+    }
+    if (placa.charCodeAt(0) == 75) {
+        nomProvincia = "Sucumbíos";
+    }
+    if (placa.charCodeAt(0) == 76) {
+        nomProvincia = "Loja";
+    }
+    if (placa.charCodeAt(0) == 77) {
+        nomProvincia = "Manabí";
+    }
+    if (placa.charCodeAt(0) == 78) {
+        nomProvincia = "Napo";
+    }
+    if (placa.charCodeAt(0) == 79) {
+        nomProvincia = "El Oro";
+    }
+    if (placa.charCodeAt(0) == 80) {
+        nomProvincia = "Pichincha";
+    }
+    if (placa.charCodeAt(0) == 81) {
+        nomProvincia = "Orellana";
+    }
+    if (placa.charCodeAt(0) == 82) {
+        nomProvincia = "Los Ríos";
+    }
+    if (placa.charCodeAt(0) == 83) {
+        nomProvincia = "Pastaza";
+    }
+    if (placa.charCodeAt(0) == 84) {
+        nomProvincia = "Tungurahua";
+    }
+    if (placa.charCodeAt(0) == 85) {
+        nomProvincia = "Cañar";
+    }
+    if (placa.charCodeAt(0) == 86) {
+        nomProvincia = "Morona Santiago";
+    }
+    if (placa.charCodeAt(0) == 87) {
+        nomProvincia = "Galápagos";
+    }
+    if (placa.charCodeAt(0) == 88) {
+        nomProvincia = "Cotopaxi";
+    }
+    if (placa.charCodeAt(0) == 89) {
+        nomProvincia = "Santa Elena";
+    }
+    if (placa.charCodeAt(0) == 90) {
+        nomProvincia = "Zamora Chincipe";
+    }
+    return nomProvincia;
 }
