21d1a39f50a54e0383e753061ac8a3abc7147ed4
martin1906s <martin.simbana007@gamil.com>
2024-08-11 13:33:01 -0500

    Reto35: Implementacion de función obtenerDiaPicoYPlaca

--- Placas/placas.js
+++ Placas/placas.js
@@ -1,21 +1,25 @@
 function validarPlaca() {
     let provincia;
     let tipo;
+    let diaPyP;
     let erroresEstructura;
     recuperarTexto("txtPlaca");
     erroresEstructura = validarEstructura("txtPlaca");
     if (erroresEstructura == null) {
         provincia = obtenerProvincia("txtPlaca")
         tipo = obtenerTipoVehiculo("txtPlaca");
+        diaPyP=obtenerDiaPicoYPlaca("txtPlaca");
         mostrarTexto("lblMensajeErrores", "PLACA: VALIDA");
         mostrarTexto("lblErrores", "");
         mostrarTexto("nomProvincia", provincia);
         mostrarTexto("tipoVehiculo", tipo);
+        mostrarTexto("diaPyP",diaPyP);
 
     } else {
         mostrarTexto("lblMensajeErrores", "PLACA: INCORRECTA");
         mostrarTexto("lblErrores", erroresEstructura);
         mostrarTexto("nomProvincia", "");
         mostrarTexto("tipoVehiculo", "");
+        mostrarTexto("diaPyP","");
     }
 }
--- Placas/servicioPlacas.js
+++ Placas/servicioPlacas.js
@@ -148,3 +148,27 @@ function obtenerTipoVehiculo(placa) {
 
     return tipoVehiculo;
 }
+
+function obtenerDiaPicoYPlaca(placa) {
+    let picoYplaca;
+    let Vplaca;
+    placa = recuperarTexto("txtPlaca");
+    Vplaca=placa.charAt(placa.length - 1);
+    if (Vplaca=="1" || Vplaca=="2") {
+        picoYplaca="Lunes"        
+    }
+    if (Vplaca=="3" || Vplaca=="4") {
+        picoYplaca="Martes"        
+    }
+    if (Vplaca=="5" || Vplaca=="6") {
+        picoYplaca="Miércoles"        
+    }
+    if (Vplaca=="7" || Vplaca=="8") {
+        picoYplaca="Jueves"        
+    }
+    if (Vplaca=="9" || Vplaca=="0") {
+        picoYplaca="Viernes"        
+    }
+
+    return picoYplaca;
+}
