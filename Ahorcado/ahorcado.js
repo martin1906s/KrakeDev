69d7cf6286164b315cfa893b9866c72162b609dc
martin1906s <martin.simbana007@gamil.com>
2024-08-13 13:17:02 -0500

    Paso 1: creación de funcion guardarPalabra

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,4 +1,6 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+
 function esMayuscula(caracter){
     let codigoAscii=caracter.charCodeAt(0);
     if(codigoAscii>=65 && codigoAscii<=90){
@@ -6,4 +8,25 @@ function esMayuscula(caracter){
     }else{
         return false;
     }
+}
+
+function guardarPalabra() {
+    let palabra=recuperarTexto("txtSecreta");
+    let letra;
+    let contadorMayusculas = 0;
+
+    if(palabra.length!=5){
+        alert("Debe ingresar una palabra de 5 letras");
+    }
+
+    for (let i = 0; i < palabra.length; i++) {
+        letra = palabra.charAt(i);
+        if (esMayuscula(letra)) {
+            contadorMayusculas++;
+        }
+    }
+    if (contadorMayusculas!=5) {
+        alert("Debe contener 5 letras mayúsculas");
+    }
+    palabraSecreta=palabra;
 }
