e5db954c96efb0a570c010a95711f59817990739
martin1906s <martin.simbana007@gamil.com>
2024-08-14 21:09:26 -0500

    Funcion mostrar empleados y creacion de 3er objeto

--- roles/roles.js
+++ roles/roles.js
@@ -1,9 +1,31 @@
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
-    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1715614788", nombre: "Alexandra", apellido: "Flores", sueldo: 1200.0 }
 ]
-
+function mostrarEmpleados() {
+    let cmpTabla = document.getElementById("tablaEmpleados")
+    let contenidoTabla = "<table border='1'><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>"+
+        "<th>SUELDO</th>"
+        + "</tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoEmpleado.cedula + "</td>"
+            + "<td>" + elementoEmpleado.nombre + "</td>"
+            + "<td>" + elementoEmpleado.apellido + "</td>"
+            +"<td>" + elementoEmpleado.sueldo + "</td>"
+            +"</tr>";
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
 function mostrarOpcionEmpleado() {
+    mostrarEmpleados()
     let divEmpleado = mostrarComponente("divEmpleado");
     let divRol = ocultarComponente("divRol");
     let divResumen = ocultarComponente("divResumen");
